fff5d3bb1fe81de2b48a2f7894c92d906192a01b
Novak <gnovak.boskov@gmail.com>
2014-12-23 19:14:33 +0100

    sa session['oauth'] u /auth/facebook v2

--- app.rb
+++ app.rb
@@ -90,8 +90,7 @@ get "/" do
 
   # Get base API Connection
   puts "--------------------------------------------------SADA TREBA DA DOBIJE GRAPH TOKENOM --------------------------------------------------"
-  #@graph  = Koala::Facebook::API.new(access_token)
-  @graph = Koala::Facebook::API.new(session[:access_token])
+  @graph  = Koala::Facebook::API.new(access_token)
 
   # Get public details of current application
   @app  =  @graph.get_object(ENV["FACEBOOK_APP_ID"])
