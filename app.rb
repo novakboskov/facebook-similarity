2f4a03de0ed00119f9503c76127b04c0fbe20984
Novak <gnovak.boskov@gmail.com>
2014-12-23 18:45:35 +0100

    benben primer ceo njegov

--- app.rb
+++ app.rb
@@ -80,6 +80,7 @@ error(Koala::Facebook::APIError) do
   redirect "/auth/facebook"
 end
 
+=begin
 get "/" do
   # testiram mongo bazu
   mongo_uri = ENV['MONGOLAB_URI']
@@ -160,5 +161,41 @@ get '/auth/facebook/callback' do
   token_string = ''
   token_string ||= session[:access_token]
   puts 'U AUTH/FACEBOOK/CALLBACK , access_token = ' + token_string
+  redirect '/'
+end
+=end
+
+get '/' do
+  if session['access_token']
+    'You are logged in! <a href="/logout">Logout</a>'
+    # do some stuff with facebook here
+    # for example:
+    # @graph = Koala::Facebook::GraphAPI.new(session["access_token"])
+    # publish to your wall (if you have the permissions)
+    # @graph.put_wall_post("I'm posting from my new cool app!")
+    # or publish to someone else (if you have the permissions too ;) )
+    # @graph.put_wall_post("Checkout my new cool app!", {}, "someoneelse's id")
+  else
+    '<a href="/login">Login</a>'
+  end
+end
+
+get '/login' do
+  # generate a new oauth object with your app data and your callback url
+  session['oauth'] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, "#{request.base_url}/callback")
+  # redirect to facebook to get your code
+  redirect session['oauth'].url_for_oauth_code()
+end
+
+get '/logout' do
+  session['oauth'] = nil
+  session['access_token'] = nil
+  redirect '/'
+end
+
+#method to handle the redirect from facebook back to you
+get '/callback' do
+  #get the access token from facebook with your code
+  session['access_token'] = session['oauth'].get_access_token(params[:code])
   redirect '/'
 end
