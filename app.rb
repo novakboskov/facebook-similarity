6fddd4b87b23b109489a697ee8af89d729412ac9
Novak <gnovak.boskov@gmail.com>
2014-12-23 20:02:47 +0100

    benbens da vidim gde 404

--- app.rb
+++ app.rb
@@ -179,7 +179,7 @@ get '/' do
     'You are logged in! <a href="/logout">Logout</a>'
     # do some stuff with facebook here
     # for example:
-    @graph = Koala::Facebook::GraphAPI.new(session["access_token"])
+    @graph = Koala::Facebook::API.new(session["access_token"])
     # publish to your wall (if you have the permissions)
     # @graph.put_wall_post("I'm posting from my new cool app!")
     # or publish to someone else (if you have the permissions too ;) )
