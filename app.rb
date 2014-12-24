0e775b8e00dd9e762d7ad58d95a9262d527c1662
Novak <gnovak.boskov@gmail.com>
2014-12-24 17:30:18 +0100

    posle tutorijala 28

--- app.rb
+++ app.rb
@@ -229,7 +229,7 @@ end
 get '/callback' do
   #get the access token from facebook with your code
   session['access_token'] = session['oauth'].get_access_token(params[:code])
-  #redirect '/'
+  redirect '/'
 end
 
 get "/auth/facebook" do
