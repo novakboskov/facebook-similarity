e94cd447ca33d05a79d76ef82116f7cf3695028c
Novak <gnovak.boskov@gmail.com>
2014-12-24 13:57:26 +0100

    posle tutorijala 8

--- app.rb
+++ app.rb
@@ -163,7 +163,7 @@ end
 
 get "/auth/facebook" do
   begin
-    @authenticator.url_for_oauth_code(:permissions => FACEBOOK_SCOPE)
+    redirect @authenticator.url_for_oauth_code(:permissions => FACEBOOK_SCOPE)
     # da vidim gde ce ovo gore da me redirektuje
   rescue
     puts "_______________greska kod /auth/facebook/callback  ____________________"
@@ -172,6 +172,11 @@ get "/auth/facebook" do
   token_string ||= session[:access_token]
   puts 'U AUTH/FACEBOOK/CALLBACK , access_token = ' + token_string
   #redirect '/'
+end
+
+get "/auth/facebook/callback" do
+  session[:access_token] = @authenticator.get_access_token(params[:code])
+  redirect '/'
 end
 
 =begin
