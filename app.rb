e5103912ab6e34ebc583c667d313b860f58e0932
Novak <gnovak.boskov@gmail.com>
2015-01-03 13:45:31 +0100

    typeerror added kod testa 2

--- app.rb
+++ app.rb
@@ -123,10 +123,10 @@ get "/calculate" do
   begin
   puts "ObjectSpace._id2ref(session[:data_thread]) = " + ObjectSpace._id2ref(session[:data_thread]).to_s\
   + " , session[:data_thread] = " + session[:data_thread].to_s
-  rescue RangeError => range_err
-    puts "RangeError kod ispisa ovog testa jer direct access to /calculate without session: " + range_err.message
-  rescue TypeError => type_err
-    puts "TypeError kod ispisa ovog testa sessin[:data_thread]=nil: " + type_err.message
+  rescue RangeError => range_errt
+    puts "RangeError kod ispisa ovog testa jer direct access to /calculate without session: " + range_errt.message
+  rescue TypeError => type_errt
+    puts "TypeError kod ispisa ovog testa sessin[:data_thread]=nil: " + type_errt.message
   end
 
   begin
@@ -139,7 +139,7 @@ get "/calculate" do
     # session[:data_thread] is not id value
     # direct access to /calculate without session
     puts "RangeError in /calculate, direct access to /calculate without session: " + range_err.message
-  rescue TypeError => type_error
+  rescue TypeError => type_err
     # session[:data_thread] is nil
     puts "TypeError in /calculate, direct access to /calculate without session: " + type_err.message
   end
